45783d8a93d850e1e29dde75c3cffc7ce6b71b10
idogross1 <idogross1@gmail.com>
2021-07-21 12:41:51 +0300

    middleware host

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -1,27 +1,36 @@
-const logger = require('../services/logger.service')
+const logger = require('../services/logger.service');
 
 function requireAuth(req, res, next) {
   if (!req.session || !req.session.user) {
-    res.status(401).end('Not authenticated, Please Login')
-    return
+    res.status(401).end('Not authenticated, Please Login');
+    return;
   }
-  next()
+  next();
 }
 
 function requireAdmin(req, res, next) {
-  const user = req.session.user
+  const user = req.session.user;
   if (!user.isAdmin) {
-    logger.warn(user.fullname + ' Attempt to perform admin action')
-    res.status(403).end('Unauthorized Enough..')
-    return
+    logger.warn(user.fullname + ' Attempt to perform admin action');
+    res.status(403).end('Unauthorized Enough..');
+    return;
   }
-  next()
+  next();
 }
-
+function requireHost(req, res, next) {
+  const user = req.session.user;
+  if (!user.isHost) {
+    logger.warn(user.fullname + ' Attempt to perform host action');
+    res.status(403).end('Unauthorized Enough..');
+    return;
+  }
+  next();
+}
 
 // module.exports = requireAuth
 
 module.exports = {
   requireAuth,
-  requireAdmin
-}
+  requireAdmin,
+  requireHost,
+};
