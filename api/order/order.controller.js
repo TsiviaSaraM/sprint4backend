04c0b52f23e42001d275fa404bedafe921644ad3
idogross1 <idogross1@gmail.com>
2021-07-25 11:50:57 +0300

    formatter

--- api/order/order.controller.js
+++ api/order/order.controller.js
@@ -1,96 +1,98 @@
-const orderService = require('./order.service')
-const socketService = require('../../services/socket.service')
-const logger = require('../../services/logger.service')
+const orderService = require('./order.service');
+const socketService = require('../../services/socket.service');
+const logger = require('../../services/logger.service');
 
 async function getOrder(req, res) {
-    try {
-        const orderId = req.params.id
-        const order = await orderService.getById(orderId)
-        res.send(order)
-    } catch (err) {
-        logger.error('Failed to get order in controller', err, req.params.id)
-        res.status(500).send({ err: 'Failed to get order' })
-    }
+  try {
+    const orderId = req.params.id;
+    const order = await orderService.getById(orderId);
+    res.send(order);
+  } catch (err) {
+    logger.error('Failed to get order in controller', err, req.params.id);
+    res.status(500).send({ err: 'Failed to get order' });
+  }
 }
 
 async function getOrders(req, res) {
-    try {
-        const filterBy = req.query;
-        // const filterBy = {
-        //     txt: req.query?.txt || '',
-        //     minBalance: +req.query?.minBalance || 0
-        // }
-        const orders = await orderService.query(filterBy)
-        res.send(orders)
-    } catch (err) {
-        logger.error('Failed to get orders', err)
-        res.status(500).send({ err: 'Failed to get orders' })
-    }
+  try {
+    const filterBy = req.query;
+    // const filterBy = {
+    //     txt: req.query?.txt || '',
+    //     minBalance: +req.query?.minBalance || 0
+    // }
+    const orders = await orderService.query(filterBy);
+    res.send(orders);
+  } catch (err) {
+    logger.error('Failed to get orders', err);
+    res.status(500).send({ err: 'Failed to get orders' });
+  }
 }
 
 async function deleteOrder(req, res) {
-    try {
-        await orderService.remove(req.params.id)
-        res.send({ msg: 'Deleted successfully' })
-    } catch (err) {
-        logger.error('Failed to delete order', err)
-        res.status(500).send({ err: 'Failed to delete order' })
-    }
+  try {
+    await orderService.remove(req.params.id);
+    res.send({ msg: 'Deleted successfully' });
+  } catch (err) {
+    logger.error('Failed to delete order', err);
+    res.status(500).send({ err: 'Failed to delete order' });
+  }
 }
 
 async function updateOrder(req, res) {
-    try {
-        const order = req.body.id
-        const savedOrder = await orderService.update(order)
-        res.send(savedOrder)
-        // socketService.broadcast({type: 'order-updated', data: order, to:savedOrder._id})
-        // return savedOrder
-    } catch (err) {
-        logger.error('Failed to update order', err)
-        res.status(500).send({ err: 'Failed to update order' })
-    }
+  try {
+    const order = req.body.id;
+    const savedOrder = await orderService.update(order);
+    res.send(savedOrder);
+    socketService.broadcast({
+      type: 'order-updated',
+      data: order,
+      to: savedOrder._id,
+    });
+    return savedOrder;
+  } catch (err) {
+    logger.error('Failed to update order', err);
+    res.status(500).send({ err: 'Failed to update order' });
+  }
 }
-
 
 async function addOrder(req, res) {
-    try {
-        const order = req.body
-        const savedOrder = await orderService.add(order)
-        res.send(savedOrder)
-        // socketService.broadcast({type: 'order-updated', data: order, to:savedOrder._id})
-    } catch (err) {
-        logger.error('Failed to update order', err)
-        res.status(500).send({ err: 'Failed to update order' })
-    }
+  try {
+    const order = req.body;
+    const savedOrder = await orderService.add(order);
+    res.send(savedOrder);
+    // socketService.broadcast({type: 'order-updated', data: order, to:savedOrder._id})
+  } catch (err) {
+    logger.error('Failed to update order', err);
+    res.status(500).send({ err: 'Failed to update order' });
+  }
 }
 
-async function addReview(){
-    try {
-        var review = req.body
-        review.byUserId = req.session.user._id
-        review = await orderService.addReview(review)
-        
-        // prepare the updated review for sending out
-        review.byUser = await userService.getById(review.byUserId)
-        review.aboutUser = await userService.getById(review.aboutUserId)
+async function addReview() {
+  try {
+    var review = req.body;
+    review.byUserId = req.session.user._id;
+    review = await orderService.addReview(review);
 
-        console.log('CTRL SessionId:', req.sessionID);
-        // socketService.broadcast({type: 'review-added', data: review})
-        // socketService.emitToAll({type: 'user-updated', data: review.byUser, room: req.session.user._id})
-        res.send(review)
+    // prepare the updated review for sending out
+    review.byUser = await userService.getById(review.byUserId);
+    review.aboutUser = await userService.getById(review.aboutUserId);
 
-    } catch (err) {
-        console.log(err)
-        logger.error('Failed to add review', err)
-        res.status(500).send({ err: 'Failed to add review' })
-    }
+    console.log('CTRL SessionId:', req.sessionID);
+    // socketService.broadcast({type: 'review-added', data: review})
+    // socketService.emitToAll({type: 'user-updated', data: review.byUser, room: req.session.user._id})
+    res.send(review);
+  } catch (err) {
+    console.log(err);
+    logger.error('Failed to add review', err);
+    res.status(500).send({ err: 'Failed to add review' });
+  }
 }
 
 module.exports = {
-    getOrder,
-    getOrders,
-    deleteOrder,
-    updateOrder,
-    addOrder,
-    addReview,
-}
+  getOrder,
+  getOrders,
+  deleteOrder,
+  updateOrder,
+  addOrder,
+  addReview,
+};
--- api/order/order.routes.js
+++ api/order/order.routes.js
@@ -1,21 +1,29 @@
-const express = require('express')
-const {requireAuth, requireAdmin} = require('../../middlewares/requireAuth.middleware')
-const {addOrder, getOrder, getOrders, deleteOrder, updateOrder} = require('./order.controller')
-const router = express.Router()
+const express = require('express');
+const {
+  requireAuth,
+  requireAdmin,
+} = require('../../middlewares/requireAuth.middleware');
+const {
+  addOrder,
+  getOrder,
+  getOrders,
+  deleteOrder,
+  updateOrder,
+} = require('./order.controller');
+const router = express.Router();
 
 // middleware that is specific to this router
 // router.use(requireAuth)
 
-//ququ how do I add filter here
-router.get('/', getOrders)
-router.get('/:id', getOrder)
+router.get('/', getOrders);
+router.get('/:id', getOrder);
 // router.put('/:id', requireAuth,  updateOrder)
-router.put('/:id', updateOrder)
+router.put('/:id', updateOrder);
 // router.post('/', requireAuth,  addOrder)
-router.post('/', addOrder)
+router.post('/', addOrder);
 // router.post('/:id/review', addReview)
 // router.put('/:id',  requireAuth, updateOrder)
 // router.delete('/:id',  requireAuth, requireAdmin, deleteOrder)
-router.delete('/:id', deleteOrder)
+router.delete('/:id', deleteOrder);
 
-module.exports = router
+module.exports = router;
